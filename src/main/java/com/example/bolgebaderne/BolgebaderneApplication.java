95e5a8a46d9df0162e43cfb9d9c9a435a9207212
Sumaya <suah0002@stud.ek.dk>
2025-12-01 12:33:37 +0100

    Initial commit

---
+++ src/main/java/com/example/bolgebaderne/BolgebaderneApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolgebaderne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgebaderneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgebaderneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bolgebaderne/BolgebaderneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolgebaderne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolgebaderneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
