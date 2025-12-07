13150504d4c54da76b84a4003ce9099d3ecb1b7a
ayan0138 <ayah0002@stud.ek.dk>
2025-12-07 23:35:28 +0100

    feat(ui): add simple role-based navigation menu

---
+++ src/main/java/com/example/bolgebaderne/controller/GlobalModelAttributes.java
@@ -0,0 +1,24 @@
+package com.example.bolgebaderne.controller;
+
+
+import com.example.bolgebaderne.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAttributes { //hjælpeklasse
+
+    @ModelAttribute("role")
+    public String addRole(@AuthenticationPrincipal User currentUser) {
+        if (currentUser == null) {
+            return "GUEST"; // ikke logget ind
+        }
+
+        // Hvis din User har en enum Role (MEMBER, NON_MEMBER, ADMIN)
+        return currentUser.getRole().name();
+
+        // Hvis feltet er en String, brug i stedet:
+        // return currentUser.getRole();
+    }
+}
